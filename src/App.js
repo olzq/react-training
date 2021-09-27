a293f06911dd39ddb0ec107f562b2a6c2f70acfc
Olzhas Kashkynbekov <olzhas_kashkynbekov@epam.com>
2021-09-27 16:31:29 +0600

    Disable linting for files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from 'react';  
 class App extends Component {  
    render(){ 
+      // eslint-disable-next-line no-undef
       const environment = process.env.NODE_ENV;
       return(  
          <div className="center">  
