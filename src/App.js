f77cd323bfcd6aa434b4b034e1b239e5017d0227
Olzhas Kashkynbekov <olzhas_kashkynbekov@epam.com>
2021-10-16 18:16:40 +0600

    Refactor App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import PropTypes from "prop-types";
+import HomePage from "./components/HomePage";
 import styled from "styled-components";
 import { MAIN_BACKGROUND_COLOR, WHITE } from "./style/constants";
 
-const Div = styled.div`
+const Wrapper = styled.div`
   background-color: ${MAIN_BACKGROUND_COLOR};
   font-family: "Roboto", sans-serif;
   color: ${WHITE};
-  padding: 52px 0px 52px 52px;
 `;
-const App = ({ children }) => <Div>{children}</Div>;
 
-App.propTypes = {
-  children: PropTypes.object.isRequired,
-};
+const App = () => (
+  <Wrapper>
+    <HomePage />
+  </Wrapper>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-import HomePage from "./components/HomePage";
 import ErrorBoundary from "./components/shared/ErrorBoundry/ErrorBoundry.js";
 import "./style/reset.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App>
-        <HomePage />
-      </App>
+      <App />
     </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById("app")
