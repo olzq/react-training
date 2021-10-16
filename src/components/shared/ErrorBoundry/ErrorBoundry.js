7828725c220931e41a7f96f04a5773a1a4fbd2ca
Olzhas Kashkynbekov <olzhas_kashkynbekov@epam.com>
2021-10-16 18:27:58 +0600

    Replace console.log with console.error

--- src/components/shared/ErrorBoundry/ErrorBoundry.js
+++ src/components/shared/ErrorBoundry/ErrorBoundry.js
@@ -11,7 +11,7 @@ class ErrorBoundary extends Component {
   }
 
   componentDidCatch(error, errorInfo) {
-    console.log(error, errorInfo);
+    console.error(error, errorInfo);
   }
 
   render() {
